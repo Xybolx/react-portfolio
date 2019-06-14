3920e24a459a3e58125868934be5b9bee8b9699d
Xybolx <xybolx@gmail.com>
2019-06-14 03:57:22 -0700

    fixing refresh issue

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.static("client/public"));
 
 // if no route is found
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/public/index.html"));
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 
 app.listen(PORT, function() {
